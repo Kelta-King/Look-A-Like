a1de83fa3ac35fc9eb5fba305c2c35ee87b7976a
Kushang Shah <kushangshah131@gmail.com>
2021-05-13 23:53:32 +0530

    Repository set

---
+++ Look_A_Like/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Look_A_Like project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Look_A_Like.settings')
+
+application = get_asgi_application()
