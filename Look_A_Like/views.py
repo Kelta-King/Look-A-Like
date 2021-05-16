1bc6383ca8bfda377587a55094a2f26f143b638e
Kushang Shah <kushangshah131@gmail.com>
2021-05-16 23:01:28 +0530

    functions and classes

--- Look_A_Like/views.py
+++ Look_A_Like/views.py
@@ -2,4 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def selectPage(request):
-    return render(request, 'selectUniverse.html')
+    
+    local_path = "../static/Images/1/AG/avengersWall.jpg"
+    
+    with open(local_path, "rb") as image_data:
+        converted_string = base64.b64encode(image_data.read())
+        print(converted_string)
+    
+    return render(request, 'selectUniverse.html', {'img':converted_string})
